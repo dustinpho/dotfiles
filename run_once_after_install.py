eedd96e0fdafa3f5cfde8ba98658b0f19f9ce730
Dustin Pho <dustinpho@gmail.com>
2025-05-02 18:24:44 -0400

    adding installation script that chezmoi can call on new setups

---
+++ run_once_after_install.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+"""thank you, chatgpt"""
+
+import os
+import subprocess
+import shutil
+import sys
+import platform
+
+
+def run(cmd, check=True, shell=False):
+    print(f"[chezmoi] Running: {' '.join(cmd) if isinstance(cmd, list) else cmd}")
+    subprocess.run(cmd, check=check, shell=shell)
+
+
+def maybe_install_brew():
+    print("[chezmoi] Checking if Homebrew is installed...")
+
+    # Check if Homebrew is already installed
+    if shutil.which("brew"):
+        print("[chezmoi] Homebrew is already installed.")
+        return
+
+    # Install Homebrew (for macOS or Linux)
+    print("[chezmoi] Installing Homebrew...")
+    if platform.system() in ["Darwin", "Linux"]:
+        run(
+            [
+                "/bin/bash",
+                "-c",
+                '"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"',
+            ],
+            shell=True,
+        )
+    else:
+        print(f"[chezmoi] unsupported OS {platform.system()=}.")
+        sys.exit(1)
+
+    assert shutil.which("brew")
+
+
+def install_packages():
+    print("[chezmoi] Installing system packages...")
+
+    maybe_install_brew()
+    run(["brew", "install", "git", "zsh", "curl", "tmux", "vim"])
+    run(["brew", "install", "--cask", "font-powerline-symbols"])
+
+
+def install_tmux_plugins():
+    print("[chezmoi] Installing Tmux Plugin Manager (TPM)...")
+    tpm_dir = os.path.expanduser("~/.tmux/plugins/tpm")
+    if not os.path.isdir(tpm_dir):
+        run(["git", "clone", "https://github.com/tmux-plugins/tpm", tpm_dir])
+
+    if shutil.which("tmux"):
+        run(["tmux", "start-server"])
+        run(["tmux", "new-session", "-d"])
+        run([os.path.join(tpm_dir, "bin/install_plugins")])
+        run(["tmux", "kill-server"])
+        print("[chezmoi] Tmux plugins installed.")
+
+
+def install_vim_plug():
+    print("[chezmoi] Installing vim-plug...")
+    plug_path = os.path.expanduser("~/.vim/autoload/plug.vim")
+    if not os.path.exists(plug_path):
+        run(
+            [
+                "curl",
+                "-fLo",
+                plug_path,
+                "--create-dirs",
+                "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
+            ]
+        )
+    run(["vim", "+PlugInstall", "+qall"])
+
+
+def install_ohmyzsh_and_p10k():
+    print("[chezmoi] Installing oh-my-zsh...")
+    omz_dir = os.path.expanduser("~/.oh-my-zsh")
+    if not os.path.isdir(omz_dir):
+        run(
+            [
+                "sh",
+                "-c",
+                "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)",
+            ],
+            shell=True,
+        )
+
+    print("[chezmoi] Installing Powerlevel10k...")
+    p10k_dir = os.path.expanduser("~/.oh-my-zsh/custom/themes/powerlevel10k")
+    if not os.path.isdir(p10k_dir):
+        run(
+            [
+                "git",
+                "clone",
+                "--depth=1",
+                "https://github.com/romkatv/powerlevel10k.git",
+                p10k_dir,
+            ]
+        )
+
+    # Change shell to zsh
+    zsh_path = shutil.which("zsh")
+    if os.environ.get("SHELL") != zsh_path and zsh_path:
+        run(["sudo", "sh", "-c", '"echo $(which zsh) >> /etc/shells"'])
+        run(["chsh", "-s", zsh_path])
+
+
+def main():
+    install_packages()
+    install_tmux_plugins()
+    install_vim_plug()
+    install_ohmyzsh_and_p10k()
+    print("[chezmoi] Setup complete. Restart your terminal.")
+
+
+if __name__ == "__main__":
+    main()
